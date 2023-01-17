585e2787c38d9f47d8984e7462334bac8423cb6a
Yudi <flixy121@gmail.com>
2023-01-17 10:52:10 +0700

    jamban

--- template.config.js
+++ template.config.js
@@ -2,5 +2,5 @@ module.exports = {
   placeholderName: 'FlixRNTemplate',
   titlePlaceholder: 'Hello App Display Name',
   templateDir: './template',
-  postInitScript: './post-init.script.sh',
+  // postInitScript: './post-init.script.sh',
 };
